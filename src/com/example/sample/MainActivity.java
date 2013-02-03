a85bcddc085b3b96ca036ae91b421435867aa3b4
kaminoson <kaminoson@gmail.com>
2013-02-04 00:12:42 +0900

    初期コミット。

---
+++ src/com/example/sample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.sample;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.NoTitle;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@NoTitle
+@EActivity(R.layout.view_main)
+public class MainActivity extends Activity {
+	//==========================================================
+	//フィールド
+
+	//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	//画面要素
+
+	@ViewById
+	Button helloButton;
+
+
+	//==========================================================
+	//メソッド
+
+	//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	//イベント
+
+	/**
+	 * 画面の初期化を行います。
+	 */
+	@AfterViews
+	void initView() {
+	}
+
+	/**
+	 * Helloボタンをクリックした時の処理を行います。
+	 */
+	@Click
+	void helloButtonClicked() {
+		Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+	}
+}
